6ae29089ca0f2f7375c437130e5e32bf9c57dc85
Maria Gomes <mariapg.dev@gmail.com>
2023-07-12 14:29:18 -0300

    Cria a função que apaga toda ou parte da expressão

--- js/main.js
+++ js/main.js
@@ -35,4 +35,28 @@ function montaExpressao(btn) {
   visorEspressao.value = expressao;
 }
 
+function apagador(btn) {
+  const visorResultado = document.querySelector('[data-visor="value"] span');
+
+  if(btn === 'AC') {
+    expressao = '';
+    visorEspressao.value = '';
+    visorEspressao.classList.remove('separador');
+  
+  
+    if(visorResultado) {
+      visor.removeChild(visorResultado);
+    }
+
+  }else if(btn ==='⌫') {
+    expressao = expressao.slice(0, visorEspressao.value.length-1);
+    visorEspressao.value = expressao;
+
+    if(visorResultado) {
+      visorEspressao.classList.remove('separador');
+      visor.removeChild(visorResultado);
+    }
+  }
+}
+
 botoes.forEach((btn) => btn.addEventListener('click', triagemDeBotoes));
