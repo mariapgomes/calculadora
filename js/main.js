96d809271d465dc0c3fdc6e2671de760ac96fa0f
Maria Gomes <mariapg.dev@gmail.com>
2023-07-18 09:03:23 -0300

    Cria a função que retorna o resultado

--- js/main.js
+++ js/main.js
@@ -67,3 +67,21 @@ function apagador(btn) {
   }
 }
 
+function resultado(expressao) {
+  const resultado = new Function('return ' + expressao)();
+
+  visorEspressao.classList.add('separador');
+  const visorResultado = document.createElement('span');
+  visorResultado.classList.add('resultado');
+  
+  if(resultado.toString(10).length > 8){
+    visorResultado.innerText = resultado.toFixed(4);
+  } else {
+    visorResultado.innerText = resultado;
+  }
+
+  visor.appendChild(visorResultado);
+}
+
+botoes.forEach((btn) => btn.addEventListener('click', triagemDeBotoes));
+
