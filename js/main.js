a96cec70eae55df63d10b9a7cb92906323b76030
Maria Gomes <mariapg.dev@gmail.com>
2023-08-02 14:43:40 -0300

    Cria a função que converte a expressão que contenha porcentagem, para uma formato calculável

--- js/main.js
+++ js/main.js
@@ -29,9 +29,16 @@ function montaExpressao(btn) {
     }
   }else if(btn === '.') {
     expressao += btn;
-  }else {
-    resultado(expressao);
-    expressao += ' ='
+
+  } else {
+    if (expressao.includes('%')){
+      calcPorcentagem(expressao);
+      expressao += ' ='
+      
+    } else {
+      resultado(expressao);
+      expressao += ' ='
+    }
   }
 
   if(expressao.length >= 15){
@@ -65,6 +72,31 @@ function apagador(btn) {
       visor.removeChild(visorResultado);
     }
   }
+}
+
+function calcPorcentagem(expressao) {
+  const porcentagem = expressao.indexOf('%');
+  let calcPorcentagem = '';
+  let arrayExpressao = expressao.split('');
+
+  for (let i = porcentagem - 2; expressao[i] !== '(' && expressao[i] !== ' ' && i >= 0; i--) {
+    calcPorcentagem += expressao[i];
+    arrayExpressao.splice(i, 1);
+  }
+
+  let valorPorcentagem = calcPorcentagem.split('').reverse().join('');
+  let porcentagemFinal = valorPorcentagem/100;
+  arrayExpressao[arrayExpressao.indexOf('%')] = '*';
+
+  if (arrayExpressao.lastIndexOf('*') - 1 === 0) {
+    arrayExpressao.unshift(porcentagemFinal);
+
+  } else {
+    arrayExpressao.splice(arrayExpressao.lastIndexOf('*') - 1, 0, porcentagemFinal);
+  }
+
+  resultado(arrayExpressao.join(''));
+
 }
 
 function resultado(expressao) {
