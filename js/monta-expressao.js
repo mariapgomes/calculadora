3dab4ef63a52c123036962364f41aeb345dc3d86
Maria Gomes <mariapg.dev@gmail.com>
2023-08-03 09:39:13 -0300

    Refatora a função que monta a expressão

--- js/monta-expressao.js
+++ js/monta-expressao.js
@@ -1,34 +1,32 @@
 function montaExpressao(btn) {
   
-  if(+btn || btn === '0') {
+  if(+btn || btn === '0' || btn === '.') {
     expressao += btn;
 
-  }else if(typeof btn === 'string' & btn !== '=' & btn !== '( )' & btn !== '.') {
+  }else if(btn !== '=' && btn !== '( )') {
     expressao += ' ' + btn + ' ';
 
   }else if(btn === '( )') {
     if(expressao.includes('(')) {
-      expressao += ')'
-    } else {
-      expressao += '('
-    }
-  }else if(btn === '.') {
-    expressao += btn;
+      expressao += ')';
 
+    } else {
+      expressao += '(';
+    }
   } else {
     if (expressao.includes('%')){
       calcPorcentagem(expressao);
-      expressao += ' ='
+      expressao += ' =';
       
     } else {
       resultado(expressao);
-      expressao += ' ='
+      expressao += ' =';
     }
   }
 
-  if(expressao.length >= 15){
+  if(expressao.length >= 14){
     visorEspressao.classList.add('f34');
-  }
+  } 
 
   visorEspressao.value = expressao;
   
